baee1e852d9b2ed7b6a535966cc696fca9e4c565
lilin90 <lilin@pingcap.com>
2019-06-21 19:08:26 +0800

    *: remove files and update scripts

--- scripts/generate_pdf.sh
+++ scripts/generate_pdf.sh
@@ -12,17 +12,31 @@ MONOFONT="WenQuanYi Micro Hei Mono"
 #_version_tag="$(date '+%Y%m%d').$(git rev-parse --short HEAD)"
 _version_tag="$(date '+%Y%m%d')"
 
-pandoc -N --toc --smart --latex-engine=xelatex \
+# default version: `pandoc --latex-engine=xelatex doc.md -s -o output2.pdf`
+# used to debug template setting error
+
+
+docs_versions=(dev v3.0 v2.1)
+
+for i in "${docs_versions[@]}"
+do
+   echo "$i"
+   version="$i/doc.md"
+   echo "$version"
+   output_path="$i/output.pdf"
+
+   pandoc -N --toc --smart --latex-engine=xelatex \
     --template=templates/template.tex \
-    --listings \
     --columns=80 \
-    -V title="TiDB 中文手册" \
+    --listings \
+    -V title="TiDB Documentation" \
     -V author="PingCAP Inc." \
     -V date="${_version_tag}" \
     -V CJKmainfont="${MAINFONT}" \
-    -V mainfont="${MAINFONT}" \
-    -V sansfont="${MAINFONT}" \
-    -V monofont="${MONOFONT}" \
+    -V fontsize=12pt \
     -V geometry:margin=1in \
     -V include-after="\\input{templates/copyright.tex}" \
-    doc.md -s -o output.pdf
+    $version -s -o $output_path
+done
+
+
